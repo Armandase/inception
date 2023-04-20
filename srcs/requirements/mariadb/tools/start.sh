d758dfc5389c199d6748111f3acfe0379125d806
Armandase <damiens.armand@gmail.com>
2023-04-20 07:23:03 -0400

    tmp maria db

--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -1,9 +1,6 @@
 mkdir -p /var/run/mysql
 touch /var/run/mysql/mysql.sock
 chmod 777 /var/run/mysql/mysql.sock
-mkdir -p /var/lib/mysql
-touch /var/lib/mysql/mysql.sock
-chmod 777 /var/lib/mysql/mysql.sock
 
 mysql_install_db
 mysqld
